911cba1ae761bf5ff4b88f60c4504e092b0a863d
suzana <antolin.suzana@gmail.com>
2022-01-21 10:22:09 +0100

    added modulo comments

--- main.py
+++ main.py
@@ -22,14 +22,34 @@ def get_random_prime():
 def primitive_root(modulo):
     required_set = set(num for num in range(1, modulo))
     for co_prime_of_modulo in range(1, modulo):
-        actual_set = set()
-        for power in range(1, modulo):
-            actual_set.add(pow(co_prime_of_modulo, power) % modulo)
+        actual_set = set()  # Here set is used so that only unique numbers should fall in
+        for power in range(1, modulo): # We are searching for a number that will be co-prime
+            actual_set.add(pow(co_prime_of_modulo, power, modulo))  # co_prime_of_modulo ^ power % modulo
         if required_set == actual_set:
             return co_prime_of_modulo
         else:
             continue
+'''
+p=7
+g=3
 
+3^0(mod 7) = 1 
+3^1(mod 7) = 3  => We are starting from here
+3^2(mod 7) = 2
+3^3(mod 7) = 6
+3^4(mod 7) = 4
+3^5(mod 7) = 5 => We end here
+
+What we got? ==>                    primitive root modulo p = 3
+Why are we searching for that? ==>  public keys and shared secret can be of any value from 1 to p-1 
+Private keys will become exponents!
+
+3^6(mod 7) = 1
+3^7(mod 7) = 3
+3^8(mod 7) = 2
+3^9(mod 7) = 6
+3^10(mod 7) = 4
+'''
 
 if __name__ == '__main__':
     # Generating private keys
@@ -40,7 +60,7 @@ if __name__ == '__main__':
 
     # Generating p-g parameters
     p = get_random_prime()  # p has to be prime nuber
-    g = primitive_root(p)  # g has to be primitive rood modulo p
+    g = primitive_root(p)  # g has to be primitive root modulo p
 
     print('\n p parameter is %d, g parameter is %d \n' % (p, g))
 
